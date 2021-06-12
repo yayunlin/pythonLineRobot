629e7eb9dbfbede20136f06312245259b429da5f
林雅芸 <yayunlin53@gmail.com>
2021-06-12 14:46:55 +0800

    a

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if '你好' in msg:
+    if '你好' in msg or 'Hi' == msg or '哈嘍' == msg:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text='你好，很高興認識你'))
